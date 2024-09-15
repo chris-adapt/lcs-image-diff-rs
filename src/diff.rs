465ea241aeed66f2876f55122edbe6cdc93f5336
Chris OR <chris@adaptagency.com>
2024-09-15 17:49:14 +0200

    upgraded image lib

--- src/diff.rs
+++ src/diff.rs
@@ -1,22 +1,33 @@
+use image::Rgba;
+
 use super::base64::encode;
 use super::lcs_diff;
 
 #[derive(Debug)]
 pub struct CompareImage {
     dimensions: (u32, u32),
-    pixels: Vec<u8>,
+    pixels: Vec<Rgba<u8>>,
 }
 
 impl CompareImage {
-    pub fn new(dimensions: (u32, u32), pixels: Vec<u8>) -> Self {
+    pub fn new(dimensions: (u32, u32), pixels: Vec<Rgba<u8>>) -> Self {
         CompareImage { dimensions, pixels }
     }
 
     pub fn create_encoded_rows(&self) -> Vec<String> {
-        self.pixels
-            .chunks(self.dimensions.0 as usize * 4)
-            .map(|chunk| encode(chunk))
-            .collect()
+        let mut rows = Vec::new();
+        let mut row = Vec::new();
+        for pixel in &self.pixels {
+            row.push(pixel.0[0]);
+            row.push(pixel.0[1]);
+            row.push(pixel.0[2]);
+            row.push(pixel.0[3]);
+            if row.len() == self.dimensions.0 as usize * 4 {
+                rows.push(encode(&row));
+                row.clear();
+            }
+        }
+        rows
     }
 }
 
